8e687384a84516b32a5951a053f86442e311626f
TJ <tshepo.ntlhokoa@gmail.com>
2017-09-25 07:28:39 +0200

    first commit

---
+++ js/app/app.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    var app = angular.module('applicationHost', ['ngRoute']); //-- the array is a list of dependendencies required by this application
+    app.config(function ($routeProvider, $locationProvider) {
+       // $locationProvider.html5Mode(true);
+        $routeProvider
+            .when('/', {
+                template: '<main></main>'
+            })
+            .when('/repos/:login', {
+                template : '<repos></repos>'
+            })
+            .otherwise({redirectTo:'/'});
+    });
+
+    angular.element(document).ready(function(){
+        angular.bootstrap(document.body, ['applicationHost']);
+    });
+    
+})();
